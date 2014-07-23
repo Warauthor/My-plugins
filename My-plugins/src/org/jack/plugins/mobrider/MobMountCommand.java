1e3e4583369e727b13dad8d6893c484e39c82c3b
Warauthor <warauthor@gmail.com>
2014-07-23 17:24:23 +0100

    Mount command added

---
+++ My-plugins/src/org/jack/plugins/mobrider/MobMountCommand.java
@@ -0,0 +1,34 @@
+package org.jack.plugins.mobrider;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+
+public class MobMountCommand implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		
+		if(sender instanceof Player && cmd.getName().equalsIgnoreCase("mm")){
+			
+			Player p = (Player) sender;
+			
+			if(p.isInsideVehicle() == false){
+				
+				for(Entity e : p.getNearbyEntities(10, 10, 10)){
+					
+					e.setPassenger(p);
+					break;
+				}
+			}
+			
+			
+		}
+
+		return false;
+	}
+
+}
